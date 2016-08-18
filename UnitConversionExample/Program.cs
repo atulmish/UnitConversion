cf6539cab51b2caf97c835d1f410e61f4349d961
stratanick <nlucas@stratajet.com>
2016-08-18 15:24:45 +0100

    Add straight forward Example project

---
+++ UnitConversionExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using UnitConversion;
+
+namespace UnitConversionExample {
+    public class Program {
+        public static void Main(string[] args) {
+            // Simple programmatic approach to conversion between units
+            var converter = new MassConverter("kg", "lbs");
+            double kg = 452d;
+            double lbs = converter.LeftToRight(kg);
+            Console.WriteLine("Weight in pounds is " + lbs);
+
+            // Converting both ways is easy
+            double originalKgs = converter.RightToLeft(lbs);
+            Console.WriteLine("Converted back: " + originalKgs);
+
+            // You may also want a simpler level of precision
+            lbs = converter.LeftToRight(kg, 2);
+            Console.WriteLine("Weight in pounds is " + lbs);
+
+
+            // You can easily customise converters to support synonyms used in business logic
+            string unitFromDatabase = "MTOW (KG)";
+            double mtowKG = 3000;
+            converter.AddSynonym("kg", "MTOW (KG)");
+            converter.UnitLeft = unitFromDatabase;
+            double mtowLBs = converter.LeftToRight(mtowKG);
+            Console.Write("MTOW (KG) in lbs is " + mtowLBs);
+
+            Console.Read();
+        }
+    }
+}
---
+++ UnitConversionExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitConversionExample")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae87678b-87b7-4c4c-b677-1e12b2273a3c")]
