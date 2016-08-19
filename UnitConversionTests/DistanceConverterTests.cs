13e30e5c1d0f32af5130b9d2f64273f21f9fccff
stratanick <nlucas@stratajet.com>
2016-08-19 18:25:44 +0100

    Add decimal place limited tests for existing converter implementations

---
+++ UnitConversionTests/DistanceConverterTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MSTest;
+using UnitConversion;
+using UnitConversion.Base;
+
+namespace UnitConversionTests {
+    [TestClass()]
+    public class DistanceConverterTests {
+        DistanceConverter converter = null;
+        
+        [TestCleanup()]
+        public void Cleanup() {
+            converter = null;
+        }
+
+        [TestMethod()]
+        public void m_m() {
+            converter = new DistanceConverter("m", "m");
+            double valL = 1;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void m_km() {
+            converter = new DistanceConverter("m", "km");
+            double valL = 1000;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void m_cm() {
+            converter = new DistanceConverter("m", "cm");
+            double valL = 1;
+            double valR = 100;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void m_mm() {
+            converter = new DistanceConverter("m", "mm");
+            double valL = 1;
+            double valR = 1000;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void m_ft() {
+            converter = new DistanceConverter("m", "feet");
+            double valL = 1;
+            double valR = 3.28084;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL, 5));
+            Assert.AreEqual(valL, converter.RightToLeft(valR, 5));
+        }
+
+        [TestMethod()]
+        public void m_yd() {
+            converter = new DistanceConverter("m", "yard");
+            double valL = 1;
+            double valR = 1.09361;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL, 5));
+            Assert.AreEqual(valL, converter.RightToLeft(valR, 5));
+        }
+
+        [TestMethod()]
+        public void m_in() {
+            converter = new DistanceConverter("m", "inch");
+            double valL = 1;
+            double valR = 39.3701;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL, 4));
+            Assert.AreEqual(valL, converter.RightToLeft(valR, 4));
+        }
+    }
+}
---
+++ UnitConversionTests/MassConverterTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MSTest;
+using UnitConversion;
+using UnitConversion.Base;
+
+namespace UnitConversionTests {
+    [TestClass()]
+    public class MassConverterTests {
+        MassConverter converter = null;
+        
+        [TestCleanup()]
+        public void Cleanup() {
+            converter = null;
+        }
+
+        [TestMethod()]
+        public void kg_kg() {
+            converter = new MassConverter("kg", "kg");
+            double valL = 1;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void kg_g() {
+            converter = new MassConverter("kg", "gram");
+            double valL = 1;
+            double valR = 1000;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void kg_lb() {
+            converter = new MassConverter("kg", "lbs");
+            double valL = 1;
+            double valR = 2.20462;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL, 5));
+            Assert.AreEqual(valL, converter.RightToLeft(valR, 5));
+        }
+
+        [TestMethod()]
+        public void kg_st() {
+            converter = new MassConverter("kg", "stone");
+            double valL = 1;
+            double valR = 0.157473;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL, 6));
+            Assert.AreEqual(valL, converter.RightToLeft(valR, 6));
+        }
+
+        [TestMethod()]
+        public void kg_oz() {
+            converter = new MassConverter("kg", "ounce");
+            double valL = 1;
+            double valR = 35.274;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL, 3));
+            Assert.AreEqual(valL, converter.RightToLeft(valR, 3));
+        }
+    }
+}
