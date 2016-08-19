2610a39a21ba9e815ec1f39891ab660962a3f9f7
stratanick <nlucas@stratajet.com>
2016-08-19 12:18:19 +0100

    Update documentation

--- UnitConversion/Readme.md
+++ UnitConversion/Readme.md
@@ -32,3 +32,19 @@ Console.Write("MTOW (KG) in lbs is " + mtowLBs);
 
 Console.Read();
 ```
+
+Converters are easy to define to contribute to
+```C#
+public class MassConverter : BaseUnitConverter {
+    public MassConverter(string leftUnit, string rightUnit) {
+        var units = new UnitFactors("kg") {
+            { new UnitFactorSynonyms("kg", "kilogram"), 1 },
+            { new UnitFactorSynonyms("gram", "g"), 1000 },
+            { new UnitFactorSynonyms("lbs", "pounds"), 2.2046226218 },
+            { new UnitFactorSynonyms("stn", "stone"), 0.157473 },
+            { "ounce", 0.157473 },
+        };
+        Instantiate(units, leftUnit, rightUnit);
+    }
+}
+```
--- UnitConversionExample/Program.cs
+++ UnitConversionExample/Program.cs
@@ -12,7 +12,7 @@ namespace UnitConversionExample {
             var converter = new MassConverter("kg", "lbs");
             double kg = 452;
             double lbs = converter.LeftToRight(kg);
-            Console.WriteLine("Weight in pounds is " + lbs);
+            Console.WriteLine("452kg in pounds is " + lbs);
 
             // Converting both ways is easy
             double originalKgs = converter.RightToLeft(lbs);
@@ -20,21 +20,22 @@ namespace UnitConversionExample {
 
             // You may also want a simpler level of precision
             lbs = converter.LeftToRight(kg, 2);
-            Console.WriteLine("Weight in pounds is " + lbs);
+            Console.WriteLine("452kg in pounds (to 2 decimal places) is " + lbs);
 
             // You can easily customise converters to support synonyms used in business logic, such as those stored on a database
-            double mtowKG = 3000;
-            converter.AddSynonym("kg", "MTOW (KG)");
-            converter.UnitLeft = "MTOW (KG)";
-            lbs = converter.LeftToRight(mtowKG);
-            Console.WriteLine("MTOW (KG) in lbs is " + lbs);
+            string myUnitName = "MTOW (KG)";
+            kg = 3000;
+            converter.AddSynonym("kg", myUnitName);
+            converter.UnitLeft = myUnitName;
+            lbs = converter.LeftToRight(kg);
+            Console.WriteLine("3000 MTOW (KG) in lbs is " + lbs);
 
             // Add a new unit with a custom conversion factor
             converter.AddUnit("Chuck Norris", 9001);
             converter.UnitRight = "Chuck Norris";
             kg = 7;
             var chucks = converter.LeftToRight(kg);
-            Console.WriteLine("7 Kg is equal to " + lbs + " chucks");
+            Console.WriteLine("7kg is equal to " + lbs + " chucks");
 
             Console.Read();
         }
