2a01651dcd24422d6485acb6e182d00f7c563670
stratanick <nlucas@stratajet.com>
2016-08-19 10:44:05 +0100

    Complete build script

--- Nuget/Pack.bat
+++ Nuget/Pack.bat
@@ -1,2 +1,9 @@
-dotnet restore "../UnitConversion/project.json"
+dotnet restore "../UnitConversion/"
+dotnet restore "../UnitConversionTests/"
+
+dotnet build "../UnitConversion/"
+dotnet build "../UnitConversionTests/"
+
+dotnet test "../UnitConversionTests/"
+
 dotnet pack "../UnitConversion/project.json" --configuration=release --output="./"
