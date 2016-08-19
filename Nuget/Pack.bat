234585f56e95ea4b9bfbce7a94cb616fc21cc5e5
stratanick <nlucas@stratajet.com>
2016-08-19 17:09:43 +0100

    Remove old build prototype

--- Nuget/Pack.bat
+++
@@ -1,9 +0,0 @@
-dotnet restore "../UnitConversion/"
-dotnet restore "../UnitConversionTests/"
-
-dotnet build "../UnitConversion/"
-dotnet build "../UnitConversionTests/"
-
-dotnet test "../UnitConversionTests/"
-
-dotnet pack "../UnitConversion/project.json" --configuration=release --output="./"
