f29a90911be9a7deff2c9590ed56337281d350d5
stratanick <nlucas@stratajet.com>
2016-10-24 15:46:22 +0100

    Lower netstandard requirements to 1.3 which supports dotnet core 1.0 and framework 451

--- UnitConversion/Properties/AssemblyInfo.cs
+++ UnitConversion/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -14,6 +15,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("18f769bb-6fb7-4fe1-aba9-eae6f5fcc21e")]
