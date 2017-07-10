0f6b9ca0a7dbd17b58b9af1f647335d61ab5cc1e
Anna Novikova <anovik@yandex.ru>
2017-07-10 16:14:00 +0300

    Converted xproj->csproj to support new format for .NET Core.

--- UnitConversionExample/Program.cs
+++ UnitConversionExample/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 using UnitConversion;
 
-namespace UnitConversionExample {
+namespace UnitConversionExample
+{
     public class Program {
         public static void Main(string[] args) {
             double kg;
--- UnitConversionTests/BaseUnitConverterTests.cs
+++ UnitConversionTests/BaseUnitConverterTests.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using MSTest;
-using UnitConversion;
 using UnitConversion.Base;
 
-namespace UnitConversionTests {
+namespace UnitConversionTests
+{
     [TestClass()]
     public class BaseUnitConverterTests {
         TestConverter converter = null;
