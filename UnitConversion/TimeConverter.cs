f882d51b48f44c86301109bdd0d7e60fa71f4469
Anna Novikova <anovik@yandex.ru>
2017-07-11 12:58:39 +0300

    TimeConverter class. Unit tests for TimeConverter.

---
+++ UnitConversion/TimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using UnitConversion.Base;
+
+namespace UnitConversion
+{
+    public class TimeConverter : BaseUnitConverter  {
+
+        UnitFactors units = new UnitFactors("s") {
+            { new UnitFactorSynonyms("s", "sec", "second"), 1 },
+            { new UnitFactorSynonyms("min", "minute"), 1d / 60 },
+            { new UnitFactorSynonyms("h", "hour"), 1d / 3600 },
+            { new UnitFactorSynonyms("d", "day"), 1d / 86400 },
+            { new UnitFactorSynonyms("y", "year"), 1d / 31536000 },
+        };
+
+        public TimeConverter(string leftUnit, string rightUnit)
+        {
+            Instantiate(units, leftUnit, rightUnit);
+        }
+        public TimeConverter()
+        {
+            Instantiate(units);
+        }
+    }
+}
---
+++ UnitConversionTests/TimeConverterTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using UnitConversion;
+
+namespace UnitConversionTests
+{
+    [TestClass()]
+    public class TimeConverterTests
+    {
+        TimeConverter converter = null;
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            converter = null;
+        }
+
+        [TestMethod()]
+        public void s_s()
+        {
+            converter = new TimeConverter("s", "s");
+            double valL = 1;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+        
+        [TestMethod()]
+        public void s_min()
+        {
+            converter = new TimeConverter("s", "minute");
+            double valL = 60;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+        
+        [TestMethod()]
+        public void s_hour()
+        {
+            converter = new TimeConverter("sec", "hour");
+            double valL = 1800;
+            double valR = 0.5;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_day()
+        {
+            converter = new TimeConverter("second", "day");
+            double valL = 3600;
+            double valR = 1d/24;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_year()
+        {
+            converter = new TimeConverter("second", "y");
+            double valL = 3600*24;
+            double valR = 1d/365;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+    }
+}
