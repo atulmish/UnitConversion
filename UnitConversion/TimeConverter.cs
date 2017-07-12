d005ee0ca68bf9aceb5e1714c9789d4161835cb8
Nick Lucas <Nick-Lucas@users.noreply.github.com>
2017-07-12 13:41:38 +0100

    Feature/add time converter (#7)
    * TimeConverter class.
    Unit tests for TimeConverter.
    
    * Add fractions of seconds and leap year to time converter

---
+++ UnitConversion/TimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using UnitConversion.Base;
+
+namespace UnitConversion {
+    public class TimeConverter : BaseUnitConverter {
+
+        UnitFactors units = new UnitFactors("s") {
+            { new UnitFactorSynonyms("s", "sec", "second"), 1 },
+
+            { new UnitFactorSynonyms("ds", "decisecond", "deci-second"), 10 },
+            { new UnitFactorSynonyms("cs", "centisecond", "centi-second"), 100 },
+            { new UnitFactorSynonyms("ms", "millisecond", "milli-second"), 1_000 },
+            { new UnitFactorSynonyms("us", "μs",  "microsecond", "micro-second"), 1_000_000 },
+            { new UnitFactorSynonyms("ns", "nanosecond", "nano-second"), 1_000_000_000 },
+
+            { new UnitFactorSynonyms("min", "minute"), 1d / 60 },
+            { new UnitFactorSynonyms("h", "hour"), 1d / 3600 },
+            { new UnitFactorSynonyms("d", "day"), 1d / 86400 },
+            { new UnitFactorSynonyms("y", "year"), 1d / 31536000 },
+            { new UnitFactorSynonyms("leap year", "leap-year"), 1d / 31622400 },
+        };
+
+        public TimeConverter(string leftUnit, string rightUnit) {
+            Instantiate(units, leftUnit, rightUnit);
+        }
+        public TimeConverter() {
+            Instantiate(units);
+        }
+    }
+}
---
+++ UnitConversionTests/TimeConverterTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using UnitConversion;
+
+namespace UnitConversionTests {
+    [TestClass()]
+    public class TimeConverterTests {
+        TimeConverter converter = null;
+
+        [TestCleanup()]
+        public void Cleanup() {
+            converter = null;
+        }
+
+        [TestMethod()]
+        public void s_s() {
+            converter = new TimeConverter("s", "s");
+            double valL = 1;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_ds() {
+            converter = new TimeConverter("s", "ds");
+            double valL = 1;
+            double valR = 10;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_cs() {
+            converter = new TimeConverter("s", "cs");
+            double valL = 1;
+            double valR = 100;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_ms() {
+            converter = new TimeConverter("s", "ms");
+            double valL = 1;
+            double valR = 1000;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_us() {
+            converter = new TimeConverter("s", "us");
+            double valL = 1;
+            double valR = 1_000_000;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_ns() {
+            converter = new TimeConverter("s", "ns");
+            double valL = 1;
+            double valR = 1_000_000_000;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_min() {
+            converter = new TimeConverter("s", "minute");
+            double valL = 60;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_hour() {
+            converter = new TimeConverter("sec", "hour");
+            double valL = 1800;
+            double valR = 0.5;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_day() {
+            converter = new TimeConverter("second", "day");
+            double valL = 3600;
+            double valR = 1d / 24;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_year() {
+            converter = new TimeConverter("second", "y");
+            double valL = 3600 * 24 * 365;
+            double valR = 1;
+
+            Assert.AreEqual(valR, converter.LeftToRight(valL));
+            Assert.AreEqual(valL, converter.RightToLeft(valR));
+        }
+
+        [TestMethod()]
+        public void s_leap_year() {
+            converter = new TimeConverter("second", "leap year");
+            double valL = 3600 * 24 * 366;
+            double valR = 1;
+            
+            // TODO: ran into a float error here. result is out by a tiny amount, unsure of best approach to correct the error
+            Assert.AreEqual(valR, Math.Round(converter.LeftToRight(valL), 8));
+            Assert.AreEqual(valL, Math.Round(converter.RightToLeft(valR), 8));
+        }
+    }
+}
